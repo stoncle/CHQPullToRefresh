7b774ab9e1b2fce1c5b5e081b776a05bf3c01f36
陈鸿强 <stoncle@chenhongqiangs-MacBook-Pro.local>
2014-10-06 15:36:42 +0800

    Initial Commit

---
+++ CHQPullToRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHQPullToRefresh
+//
+//  Created by 陈鸿强 on 10/6/14.
+//  Copyright (c) 2014 陈鸿强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHQPullToRefresh/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CHQPullToRefresh
+//
+//  Created by 陈鸿强 on 10/6/14.
+//  Copyright (c) 2014 陈鸿强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
