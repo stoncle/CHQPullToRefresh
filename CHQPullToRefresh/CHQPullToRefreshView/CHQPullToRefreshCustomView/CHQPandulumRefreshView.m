a0beec34b48cc3e3ad4936787012380d9242ec8b
stoncle <stoncle100@gmail.com>
2014-10-30 20:26:01 +0800

    add pandulum theme

---
+++ CHQPullToRefresh/CHQPullToRefreshView/CHQPullToRefreshCustomView/CHQPandulumRefreshView.m
@@ -0,0 +1,21 @@
+//
+//  CHQPandulumRefreshView.m
+//  CHQPullToRefresh
+//
+//  Created by 陈鸿强 on 10/30/14.
+//  Copyright (c) 2014 陈鸿强. All rights reserved.
+//
+
+#import "CHQPandulumRefreshView.h"
+
+@implementation CHQPandulumRefreshView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
